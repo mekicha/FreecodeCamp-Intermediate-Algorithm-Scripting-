b9b1223df0b9bd4e492f023c1fd33932fa7de102
mekicha <emeka.icha@gmail.com>
2016-08-16 16:14:30 +0300

    Search and Replace

---
+++ searchAndReplace.js
@@ -0,0 +1,28 @@
+/*** Perform a search and replace on the sentence using the 
+**** arguments provided and return the new sentence.
+
+**** First argument is the sentence to perform the search and 
+**** replace on.
+
+**** Second argument is the word that you will be replacing 
+**** (before).
+
+**** Third argument is what you will be replacing the second
+****  argument with (after).
+
+**** NOTE: Preserve the case of the original word when you are 
+**** replacing it. For example if you mean to replace the 
+**** word "Book" with the word "dog", it should be replaced
+****  as "Dog"
+***/
+
+function myReplace(str, before, after) {
+	var ch = before.charAt(0);
+	if(ch === ch.toUpperCase()){
+		after = after[0].toUpperCase() + after.slice(1);
+	}
+	var res = str.replace(before, after);
+	return res;
+}
+
+myReplace("His name is Tom", "Tom", "john");
